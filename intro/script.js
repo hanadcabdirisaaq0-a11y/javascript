61b57526a6dbaba9102bfaa835ab54673b5e9fa7
hanadcabdirisaaq <hanadcabdirisaaq0@gamil.com>
2025-12-24 09:23:02 +0300

    intro

---
+++ intro/script.js
@@ -0,0 +1,15 @@
+// const para = document.queryselector(".error");
+
+// console.log(para);
+
+const para = document.getElementsByTagName("p");
+
+ console.log("para")
+
+const title = document.getElementsByClassName("hanad")
+console.log(title)
+
+
+
+
+
